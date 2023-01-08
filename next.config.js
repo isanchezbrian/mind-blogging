9d5650b2ea732792cf836363af3d31ad0c68d69b
brians97 <isanchez.brian@gmail.com>
2023-01-07 22:38:13 -0800

    config debugging

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
 
-module.exports = {
-  env : {
-    NEXT_PUBLIC_API_KEY: process.env.apiKey,
-    NEXT_PUBLIC_AUTH_DOMAIN: process.env.authDomain,
-    NEXT_PUBLIC_PROJECT_ID: process.env.projectId,
-    NEXT_PUBLIC_STORAGE_BUCKET: process.env.storageBucket,
-    NEXT_PUBLIC_MESSAGING_SENDER_ID: process.env.messagingSenderId,
-    NEXT_PUBLIC_APP_ID: process.env.appId,
-  }
-}
-
-nextConfig
+module.exports = nextConfig
