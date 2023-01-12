b6b257d173e69d7da63a82384bcb78828f238d44
brians97 <isanchez.brian@gmail.com>
2023-01-11 18:33:06 -0800

    added authentication through email and password

--- utils/firebase.js
+++ utils/firebase.js
@@ -9,12 +9,12 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_APIKEY + "",
-  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN + "",
-  projectId: process.env.NEXT_PUBLIC_PROJECTID + "",
-  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET + "",
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDER_ID = "",
-  appId: process.env.NEXT_PUBLIC_APPID = "",
+  apiKey: process.env.NEXT_PUBLIC_APIKEY + "" ,
+  authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN + "" ,
+  projectId: process.env.NEXT_PUBLIC_PROJECTID + "" ,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGEBUCKET + "" ,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGINGSENDER_ID + "" ,
+  appId: process.env.NEXT_PUBLIC_APPID + "" ,
 };
 
 // Initialize Firebase
